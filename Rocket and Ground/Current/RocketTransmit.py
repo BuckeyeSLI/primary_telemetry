5f8c8bb572749666065160eb54cb07d91078f2a7
Axodah <mdeavers77@gmail.com>
2018-01-28 17:51:50 -0500

    added comments to rocket script

--- Rocket and Ground/Current/RocketTransmit.py
+++ Rocket and Ground/Current/RocketTransmit.py
@@ -9,7 +9,10 @@ from datetime import datetime
 # call structure: python Rocket.py groundIP groundPort
 # sample call:    python Rocket.py "192.168.0.1" 7500
 # proper structure is to place this in the same directory as the log files directory
+
 #-------- INITIALIZATION ---------------------
+
+# Returns unique logfile path in logData
 def getLogPath ():
     # get unique file name
     initialPath = "flightCode/logData"
@@ -29,6 +32,28 @@ def getLogPath ():
             nameInvalid = 0
 
     return localPath
+    
+# Performs rs-232 communication  
+def getData(clusterNum):
+	data = 0
+	# Perform async call to the rs-232 port associated with clusterNum
+	# assign cluster numbers
+	# check if data, skip if none, else read data, timeout (below in a function, call serialRead)
+		#read 1 byte at a time, parsing data
+	# if timeout value, data = -1
+	# perform asyc read, if fail, increment counter (reset if success), if fail count too large, reset count and issue restart to cluster
+	
+	finalData = parseData(data)
+	return finalData
+	
+def finalData (data):
+	# if data = 0, set cluster not ready message
+	# if data = -1, set error message
+	# else de-encapsulate
+		# If gateworks needs to process id, parse and handle w/ functions
+		# prep for transmit
+	return finalData
+	
 #----------------------------------------------------
 
 #-------- MAIN ----------------------------
@@ -49,7 +74,8 @@ def main():
     localFile = open(localPath, "wb+")
     # create and bind socket to port
     speaker = socket(AF_INET,SOCK_DGRAM)
-
+	# Initialize microcontrollers
+	
     #TODO - alive test, handshake?
     #TODO - section for additional functions/socket declarations for sending commands.
     #-------- TRANSMIT ----------------------------
@@ -57,10 +83,11 @@ def main():
         # get data to transmit
         dt = datetime.now()
         msg = str(dt.hour)+":"+str(dt.minute)+":"+str(dt.second)+":"+str(dt.microsecond)
-
+		
+		#iterate ports,
 
         # encode transmission in variable teleData (string, encoded in ascii)
-        teleData = msg.encode()
+        teleData = msg.encode() #TODO encode as UTF-8 (binary?)
 
         # write data to file TODO
         writing = msg + "\n"
