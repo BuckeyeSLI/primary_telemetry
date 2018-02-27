99b6c44dd016f2634d2735c47b600af040718d18
Andrew Bennett <admin@andrewbennet1.com>
2018-02-27 18:46:15 -0500

    Implemented FIFO buffer for usart I/O

--- Sensor Clusters/Base Sensor Cluster/usart.c
+++ Sensor Clusters/Base Sensor Cluster/usart.c
@@ -1,122 +1,173 @@
 #include "usart.h"
 
 // Define USART data buffers
-static IOBuffer C0_buffer;
-static IOBuffer C1_buffer;
-static IOBuffer D0_buffer;
-static IOBuffer D1_buffer;
-static IOBuffer E0_buffer;
-static IOBuffer IN_buffer;
+static volatile FIFOBuffer C0_out_buffer;
+static volatile FIFOBuffer C1_out_buffer;
+static volatile FIFOBuffer D0_out_buffer;
+static volatile FIFOBuffer D1_out_buffer;
+static volatile FIFOBuffer E0_out_buffer;
+static volatile FIFOBuffer C0_in_buffer;
+static volatile FIFOBuffer C1_in_buffer;
+static volatile FIFOBuffer D0_in_buffer;
+static volatile FIFOBuffer D1_in_buffer;
+static volatile FIFOBuffer E0_in_buffer;
+
+// Internal function to write to FIFO buffer. Returns 1 if buffer is full.
+static uint8_t FIFOWrite(FIFOBuffer buffer, uint8_t byte)
+{
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) // Ensure buffer is accessed atomically
+	{
+		if(buffer.buffer_head + 1 == buffer.buffer_tail || (buffer.buffer_head == USART_BUFFER_SIZE - 1 && buffer.buffer_tail == 0)) return 1; // No room in buffer
+		buffer.buffer[buffer.buffer_head] = byte;
+		if(buffer.buffer_head == USART_BUFFER_SIZE - 1) buffer.buffer_head = 0; // Handle buffer wrap-around
+		else buffer.buffer_head++;
+		buffer.buffer_size++;
+	}
+	return 0;
+}
+
+// Internal function to read from FIFO buffer. Returns 1 if buffer is full.
+static uint8_t FIFORead(FIFOBuffer buffer, uint8_t *byte)
+{
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) // Ensure buffer is accessed atomically
+	{
+		if(buffer.buffer_head == buffer.buffer_tail) return 1; // Buffer empty
+		*byte = buffer.buffer[buffer.buffer_tail];
+		if(buffer.buffer_tail == USART_BUFFER_SIZE - 1) buffer.buffer_tail = 0; // Handle buffer wrap-around
+		else buffer.buffer_tail++;
+		buffer.buffer_size--;
+	}
+	return 0;
+}
 
 uint8_t USART_OutputBufferAdd(uint8_t usart, uint8_t data[], uint8_t data_size)
 {
 	switch(usart)
 	{
-		case 1:
-			if(128 - (C0_buffer.buffer_index - 1) <= data_size) return 2;  // Return 2 if not enough room in buffer
+		uint8_t error;
+		case 0:
 			for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
 			{
-				C0_buffer.buffer[C0_buffer.buffer_index + i] = data[i];
-				C0_buffer.buffer_index++;
+				error = FIFOWrite(C0_out_buffer, data[i]);
+				if(error == 1) return 2; // Return 2 if no room in buffer
 			}
-			if((C0_buffer.buffer_index) == data_size)  // Start shifting data into hardware buffers if hardware buffers are empty
+			if(C0_out_buffer.buffer_size == data_size)  // Start shifting data into hardware buffers if hardware buffers are empty
 			{
-				USART_SendByte(1);
+				USART_SendByte(0);
+			}
+			return 0;
+		case 1:
+			for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
+			{
+				error = FIFOWrite(C1_out_buffer, data[i]);
+				if(error == 1) return 2; // Return 2 if no room in buffer
+			}
+			if(C1_out_buffer.buffer_size == data_size)  // Start shifting data into hardware buffers if hardware buffers are empty
+			{
+				USART_SendByte(0);
 			}
 			return 0;
 		case 2:
-			if(128 - (C1_buffer.buffer_index - 1) <= data_size) return 2;  // Return 2 if not enough room in buffer
 			for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
 			{
-				C1_buffer.buffer[C1_buffer.buffer_index + i] = data[i];
-				C1_buffer.buffer_index++;
+				error = FIFOWrite(D0_out_buffer, data[i]);
+				if(error == 1) return 2; // Return 2 if no room in buffer
 			}
-			if((C0_buffer.buffer_index) == data_size)  // Start shifting data into hardware buffers if hardware buffers are empty
+				if(D0_out_buffer.buffer_size == data_size)  // Start shifting data into hardware buffers if hardware buffers are empty
 			{
-				USART_SendByte(2);
+				USART_SendByte(0);
 			}
 			return 0;
 		case 3:
-			if(128 - (D0_buffer.buffer_index - 1) <= data_size) return 2;  // Return 2 if not enough room in buffer
 			for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
 			{
-			D0_buffer.buffer[D0_buffer.buffer_index + i] = data[i];
-			D0_buffer.buffer_index++;
+				error = FIFOWrite(D1_out_buffer, data[i]);
+				if(error == 1) return 2; // Return 2 if no room in buffer
 			}
-			if((C0_buffer.buffer_index) == data_size)  // Start shifting data into hardware buffers if hardware buffers are empty
+			if(D1_out_buffer.buffer_size == data_size)  // Start shifting data into hardware buffers if hardware buffers are empty
 			{
-				USART_SendByte(3);
+				USART_SendByte(0);
 			}
 			return 0;
 		case 4:
-			if(128 - (D1_buffer.buffer_index - 1) <= data_size) return 2;  // Return 2 if not enough room in buffer
 			for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
 			{
-				D1_buffer.buffer[D1_buffer.buffer_index + i] = data[i];
-				D1_buffer.buffer_index++;
+				error = FIFOWrite(E0_out_buffer, data[i]);
+				if(error == 1) return 2; // Return 2 if no room in buffer
 			}
-			if((C0_buffer.buffer_index) == data_size)  // Start shifting data into hardware buffers if hardware buffers are empty
+			if(E0_out_buffer.buffer_size == data_size)  // Start shifting data into hardware buffers if hardware buffers are empty
 			{
-				USART_SendByte(4);
+				USART_SendByte(0);
 			}
 			return 0;
-		case 5:
-			if(128 - (E0_buffer.buffer_index - 1) <= data_size) return 2;  // Return 2 if not enough room in buffer
-			for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
-			{
-				E0_buffer.buffer[E0_buffer.buffer_index + i] = data[i];
-				E0_buffer.buffer_index++;
-			}
-			if((C0_buffer.buffer_index) == data_size)  // Start shifting data into hardware buffers if hardware buffers are empty
-			{
-				USART_SendByte(5);
-			}
-		return 0;
 	}
 	return 1;  // Return 1 if 'usart' is invalid
 }
 
-uint8_t USART_InputBufferAdd(uint8_t data)
+uint8_t USART_InputBufferAdd(uint8_t usart, uint8_t data)
 {
-	if(128 - (IN_buffer.buffer_index - 1) <= 1) return 1;  // Return 1 if not enough room in buffer
-	IN_buffer.buffer[IN_buffer.buffer_index] = data;
-	IN_buffer.buffer_index++;
-	return 0;
+	switch(usart)
+	{
+		uint8_t error;
+		case 0:
+			error = FIFOWrite(C0_in_buffer, data);
+			if(error == 1) return 2; // Return 2 if buffer full
+			return 0;
+		case 1:
+			error = FIFOWrite(C1_in_buffer, data);
+			if(error == 1) return 2; // Return 2 if buffer full
+			return 0;
+		case 2:
+			error = FIFOWrite(D0_in_buffer, data);
+			if(error == 1) return 2; // Return 2 if buffer full
+			return 0;
+		case 3:
+			error = FIFOWrite(D1_in_buffer, data);
+			if(error == 1) return 2; // Return 2 if buffer full
+			return 0;
+		case 4:
+			error = FIFOWrite(E0_in_buffer, data);
+			if(error == 1) return 2; // Return 2 if buffer full
+			return 0;
+	}
+	return 1;  // Return 1 if 'usart' is invalid
 }
 
 uint8_t USART_SendByte(uint8_t usart)
 {
 	switch(usart)
 	{
+		uint8_t error;
+		uint8_t data;
+		case 0:
+			if (!(USARTC0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
+			error = FIFORead(C0_out_buffer, &data);
+			if (error == 1) return 3; // Return 3 if buffer is empty
+			USARTC0.DATA = data;
+			return 0;
 		case 1:
 			if (!(USARTC0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
-			if (C0_buffer.buffer_index == 0) return 3;         // Return 3 if buffer is empty
-			USARTC0.DATA = C0_buffer.buffer[C0_buffer.buffer_index - 1];
-			C0_buffer.buffer_index--;
+			error = FIFORead(C1_out_buffer, &data);
+			if (error == 1) return 3; // Return 3 if buffer is empty
+			USARTC0.DATA = data;
 			return 0;
 		case 2:
-			if (!(USARTC1.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
-			if (C1_buffer.buffer_index == 0) return 3;         // Return 3 if buffer is empty
-			USARTC1.DATA = C1_buffer.buffer[C1_buffer.buffer_index - 1];
-			C1_buffer.buffer_index--;
+			if (!(USARTC0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
+			error = FIFORead(D0_out_buffer, &data);
+			if (error == 1) return 3; // Return 3 if buffer is empty
+			USARTC0.DATA = data;
 			return 0;
 		case 3:
-			if (!(USARTD0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
-			if (D0_buffer.buffer_index == 0) return 3;         // Return 3 if buffer is empty
-			USARTC0.DATA = D0_buffer.buffer[C0_buffer.buffer_index - 1];
-			D0_buffer.buffer_index--;
+			if (!(USARTC0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
+			error = FIFORead(D1_out_buffer, &data);
+			if (error == 1) return 3; // Return 3 if buffer is empty
+			USARTC0.DATA = data;
 			return 0;
 		case 4:
-			if (!(USARTD1.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
-			if (D1_buffer.buffer_index == 0) return 3;         // Return 3 if buffer is empty
-			USARTC0.DATA = D1_buffer.buffer[D1_buffer.buffer_index - 1];
-			D1_buffer.buffer_index--;
-			return 0;
-		case 5:
-			if (!(USARTE0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
-			if (E0_buffer.buffer_index == 0) return 3;         // Return 3 if buffer is empty
-			USARTC0.DATA = E0_buffer.buffer[E0_buffer.buffer_index - 1];
-			E0_buffer.buffer_index--;
+			if (!(USARTC0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
+			error = FIFORead(E0_out_buffer, &data);
+			if (error == 1) return 3; // Return 3 if buffer is empty
+			USARTC0.DATA = data;
 			return 0;
 	}
 	return 1;  // Return 1 if 'usart' invalid
--- Sensor Clusters/Base Sensor Cluster/usart.h
+++ Sensor Clusters/Base Sensor Cluster/usart.h
@@ -3,17 +3,25 @@
 
 #include <avr/io.h>
 #include <stdint.h>
+#include <util/atomic.h>
+
+// USART I/O buffer length - define in main.c to override
+#ifndef USART_BUFFER_SIZE
+#define USART_BUFFER_SIZE 128
+#endif
 
 // Data buffer struct definition
 typedef struct
 {
-	uint8_t buffer[128];
-	uint8_t buffer_index;
-} IOBuffer;
+	uint8_t buffer[USART_BUFFER_SIZE];
+	uint8_t buffer_head;
+	uint8_t buffer_tail;
+	uint8_t buffer_size;
+} FIFOBuffer;
 
-/*	Adds an array of data to the specified USART's output buffer.
+/*  Adds an array of data to the specified USART's output buffer.
  *  INPUTS:
- *		'usart'     - USART buffer the data should be added to. Valid values are 1-5, representing USARTs C0, C1, D0, D1, and E0 in order.
+ *		'usart'     - USART buffer the data should be added to. Valid values are 0-4, representing USARTs C0, C1, D0, D1, and E0 in order
  *		'data[]'    - Data to be added to the buffer
  *		'data_size' - Length of the array passed to 'data[]'
  *  RETURN:
@@ -25,21 +33,23 @@ uint8_t USART_OutputBufferAdd(uint8_t usart, uint8_t data[], uint8_t data_size);
 
 /*	Adds a byte of data to the USART input buffer. Meant to be called during USART receive ISR.
  *  INPUTS:
- *		'data' - Data to be added to the buffer
+ *		'usart' - USART buffer the data should be added to. Valid values are 0-4, representing USARTs C0, C1, D0, D1, and E0 in order
+ *		'data'  - Data to be added to the buffer
  *  RETURN:
  *		0 - (NO ERROR) Data added to USART buffer successfully
- *		1 - (ERROR) Not enough room in USART buffer to fit 'data'
+ *      1 - (ERROR) Invalid value passed to 'usart'
+ *		2 - (ERROR) USART buffer full
  */
-uint8_t USART_InputBufferAdd(uint8_t data);
+uint8_t USART_InputBufferAdd(uint8_t usart, uint8_t data);
 
-/*	Sends a byte of data from a USART software output buffer to the USART hardware output buffer. Meant to be called during USART send ISR.
+/*	Sends a byte of data from a USART software output buffer to a USART hardware output buffer. Meant to be called during USART send ISR.
  *  INPUTS:
  *		'usart' - USART buffer to shift data byte from software to hardware
  *  RETURN:
  *		0 - (NO ERROR) Data added to USART hardware buffer successfully
  *		1 - (ERROR) Invalid value passed to 'usart'
  *		2 - (ERROR) USART hardware buffer not ready
- *		3 - (ERROR) Not enough room in USART buffer to fit 'data'
+ *		3 - (ERROR) USART output buffer is empty
  */
 uint8_t USART_SendByte(uint8_t usart);
 
